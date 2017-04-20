2511661ed85e0978efe181673ebd4544d63b23b7
Thierry Delamare <t.delamare@laposte.net>
2017-04-20 15:16:53 +0200

    Adds gpg-agent env helper

--- helper.mk
+++ helper.mk
@@ -56,7 +56,8 @@ help += self-help
 
 define env
 echo 'echo $$PATH | grep ":\.:*" | line > /dev/null || export PATH=$$PATH:.';
-echo 'export SSH_AUTH_SOCK=/$$(sudo lsof -a -U -u $$USER -c ssh-agent -Fn -w | tail -1 | cut -d/ -f2-)';
+echo 'source <(< ~/.gpg-agent-info xargs -i echo export {})'
+echo 'echo export SSH_AUTH_SOCK=/$$(sudo lsof -a -U -u $$USER -c ssh-agent -Fn -w | tail -1 | cut -d/ -f2-)';
 echo 'export TERM=eterm-color';
 echo 'export GIT_PAGER=cat';
 echo "export GIT_EDITOR='emacsclient -s epi -c'";
