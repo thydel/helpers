d5cf39627c24fdf112519810f49ab88798eb4cad
Thierry Delamare <t.delamare@laposte.net>
2018-08-08 17:15:40 +0200

    Adds parallel to git helper

--- helper.mk
+++ helper.mk
@@ -247,6 +247,8 @@ echo "ls -d */.git | cut -d/ -f1 | xargs -i echo echo {}\; git -C {} status -sb 
 echo "ls -d */.git | cut -d/ -f1 | xargs -i echo echo {}\; git -C {} fetch | dash";
 echo "ls -d */.git | cut -d/ -f1 | xargs -i echo echo {}\; git -C {} pull | dash";
 echo "ls -d */.git | cut -d/ -f1 | xargs -i echo git-dates run dates repo={} | dash";
+echo;
+echo "parallel echo git {2} {1} master ::: manin wato  ::: pull push"
 endef
 help += git
 
