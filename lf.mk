870b83f453fd358fb05c0b9553c54a6ebd6c3883
Thierry Delamare <t.delamare@laposte.net>
2021-03-30 19:22:21 +0200

    Adds lf.{mk,sh}

---
+++ lf.mk
@@ -0,0 +1,3 @@
+#!/usr/bin/make -f
+
+/usr/local/bin/lf: lf.sh; install $< $@
---
+++ lf.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+fail() { unset fail; : "${fail:?$@}"; }
+check-tty () { [ -t 0 ] && fail "input shouldn't be a TTY"; }
+
+github-file-content () { xargs -ri gh api repos/:owner/:repo/contents/{}${1+?ref=$1}; }
+github-url-to-markdown () { jq -r '"\n    " as $s | "[\(.path)]:\($s)\(._links.html)\($s)\"github.com file\""'; }
+github-file-to-markdown () { check-tty; github-file-content "$@" | github-url-to-markdown; }
+alias ghf2url=github-file-to-markdown
+
+git-repo () { git config --get remote.origin.url | cut -d: -f2 | cut -d. -f-1; }
+git-branch-or-tag () { if [ "$1" ]; then git tag | grep $1 || fail no $1 tag; else git branch --show-current; fi; }
+git-file-to-url() { xargs -ri echo https://github.com/$(git-repo)/blob/$(git-branch-or-tag "$@")/{}; }
+git-url-to-markdown () { jq -Rr '"\n    " as $s | split("/")[-1] as $p | "[\($p)]:\($s)\(.)\($s)\"github.com file\""'; }
+git-file-to-markdown () { check-tty; git-file-to-url "$@" | git-url-to-markdown; }
+alias gf2url2=git-file-to-markdown
+
+declare -f
+declare -F | awk '{ print $NF }' | xargs echo export -f
+alias
