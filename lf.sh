209c5238c9fe768251377e5c3a0fb6c4237ffbb0
Thierry Delamare <t.delamare@laposte.net>
2021-04-01 00:46:05 +0200

    Adds github-commit-to-md

--- lf.sh
+++ lf.sh
@@ -3,17 +3,32 @@
 fail() { unset fail; : "${fail:?$@}"; }
 check-tty () { [ -t 0 ] && fail "input shouldn't be a TTY"; }
 
+jq-md-url () { jq --argjson s '"\n    "' -r "$1"; }
+
+github-files () { gh api /repos/:owner/:repo/git/trees/$(git branch --show-current)?recursive=1 | jq -r '.tree|map(.path)[]'; }
+
 github-file-content () { xargs -ri gh api repos/:owner/:repo/contents/{}${1+?ref=$1}; }
-github-url-to-markdown () { jq -r '"\n    " as $s | "[\(.path)]:\($s)\(._links.html)\($s)\"github.com file\""'; }
+github-url-to-markdown () { jq-md-url '"[\(.path)]:\($s)\(._links.html)\($s)\"github.com file\""'; }
 github-file-to-markdown () { check-tty; github-file-content "$@" | github-url-to-markdown; }
 alias ghf2url=github-file-to-markdown
 
-git-repo () { git config --get remote.origin.url | cut -d: -f2 | cut -d. -f-1; }
+git-url () { git config --get remote.origin.url; }
+git-repo () { git-url | cut -d: -f2 | cut -d. -f-1; }
+git-repo () { git-url | jq -Rr 'split(":")[1]|split(".")[:-1][]'; }
+git-site () { git-url | jq -Rr 'split(":")[0]|split("@")[1]'; }
 git-branch-or-tag () { if [ "$1" ]; then git tag | grep $1 || fail no $1 tag; else git branch --show-current; fi; }
-git-file-to-url() { xargs -ri echo https://github.com/$(git-repo)/blob/$(git-branch-or-tag "$@")/{}; }
-git-url-to-markdown () { jq -Rr '"\n    " as $s | split("/")[-1] as $p | "[\($p)]:\($s)\(.)\($s)\"github.com file\""'; }
+git-file-to-url() { xargs -ri echo https://$(git-site)/$(git-repo)/blob/$(git-branch-or-tag "$@")/{}; }
+git-url-to-markdown () { jq -R | jq-md-url 'split("/")[-1] as $p | "[\($p)]:\($s)\(.)\($s)\"github.com file\"\n"'; }
 git-file-to-markdown () { check-tty; git-file-to-url "$@" | git-url-to-markdown; }
 alias gf2url=git-file-to-markdown
+
+github-repo-to-js () { gh api repos/:owner/:repo | jq '{ name, user: .owner.login, url: .html_url, type: (if .private then "private" else "public" end) }'; }
+git-commit-to-js () { git log -${1:-1} --pretty='{ "comment": "%s", "commit": "%H" }'; }
+github-repo-and-commit-to-js () { (github-repo-to-js; git-commit-to-json "$@") | jq -n 'input as $r | [inputs] | map([$r, .] | add)[]'; }
+
+github-repo-to-md () { github-repo-to-js | jq-md-url '"[\(.name)]:\($s)\(.url)\($s)\"github.com \(.type) repo\""'; }
+github-commit-to-md () { github-repo-and-commit-to-js "$@" | jq-md-url '"[\(.comment)]:\($s)\(.url)/commit/\(.commit)\($s)\"github.com commit\"\n"'; }
+alias ghc2dm=github-commit-to-md
 
 declare -f
 declare -F | awk '{ print $NF }' | xargs echo export -f
