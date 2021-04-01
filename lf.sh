c4c1830cb395895cd32fc81259c604ecd72194ee
Thierry Delamare <t.delamare@laposte.net>
2021-04-01 18:15:01 +0200

    Corrects names

--- lf.sh
+++ lf.sh
@@ -10,7 +10,7 @@ github-files () { gh api /repos/:owner/:repo/git/trees/$(git branch --show-curre
 github-file-content () { xargs -ri gh api repos/:owner/:repo/contents/{}${1+?ref=$1}; }
 github-url-to-markdown () { jq-md-url '"[\(.path)]:\($s)\(._links.html)\($s)\"github.com file\""'; }
 github-file-to-markdown () { check-tty; github-file-content "$@" | github-url-to-markdown; }
-alias ghf2url=github-file-to-markdown
+alias ghf2md=github-file-to-markdown
 
 git-url () { git config --get remote.origin.url; }
 git-repo () { git-url | cut -d: -f2 | cut -d. -f-1; }
@@ -20,7 +20,7 @@ git-branch-or-tag () { if [ "$1" ]; then git tag | grep $1 || fail no $1 tag; el
 git-file-to-url() { xargs -ri echo https://$(git-site)/$(git-repo)/blob/$(git-branch-or-tag "$@")/{}; }
 git-url-to-markdown () { jq -R | jq-md-url 'split("/")[-1] as $p | "[\($p)]:\($s)\(.)\($s)\"github.com file\"\n"'; }
 git-file-to-markdown () { check-tty; git-file-to-url "$@" | git-url-to-markdown; }
-alias gf2url=git-file-to-markdown
+alias gf2md=git-file-to-markdown
 
 github-repo-to-js () { gh api repos/:owner/:repo | jq '{ name, user: .owner.login, url: .html_url, type: (if .private then "private" else "public" end) }'; }
 git-commit-to-js () { git log -${1:-1} --pretty='{ "comment": "%s", "commit": "%H" }'; }
@@ -34,7 +34,7 @@ alias ghc2md=github-commit-to-md
 
 relative-file-to-js () { jq -R '{ url: ., name: split(".")[0] | split("_")[2] }'; }
 relative-file-to-md () { check-tty; relative-file-to-js | jq-md-url '"[\(.name)]:\($s)\(.url)\($s)\"github.com relative file\"\n"'; }
-alias rf2md=relative-file-to-md
+alias ghrf2md=relative-file-to-md
 
 declare -f
 declare -F | awk '{ print $NF }' | xargs echo export -f
