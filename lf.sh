632c175a642cdcd8e733b25706448a4700a6924a
Thierry Delamare <t.delamare@laposte.net>
2021-04-03 01:09:01 +0200

    Adds more meta funcs

--- lf.sh
+++ lf.sh
@@ -36,20 +36,35 @@ relative-file-to-js () { jq -R '{ url: ., name: split(".")[0] | split("_")[2] }'
 relative-file-to-md () { check-tty; relative-file-to-js | jq-md-url '"[\(.name)]:\($s)\(.url)\($s)\"github.com relative file\"\n"'; }
 alias ghrf2md=relative-file-to-md
 
-run-func () { : ${1:?}; declare -f $1; echo "$@"; }
-ssh-run-func () { : ${2:?}; local n=$1; shift; run-func "$@" | ssh $n ${RUN:-bash}; }
-alias srf=ssh-run-func
-
 list () { fmt -1; }
 args () { tr '\n' ' '; }
 apply () { : ${1:?}; read; ${ECHO:+echo} "$@" $REPLY; }
 map () { : ${1:?}; while read; do "$@" ${ECHO:+echo} $REPLY; done; }
 
-func-on-a-line () { declare -f ${1:?} | tac | sed -re '2s/$/;/' -e 's/^ +//' -e 's/ +$//' | tac | args; echo; }
+func-name () { echo ${BASH_ALIASES[${1:?}]:-$1}; }
+show-func-maybe-export () { func-name $1 | { read f; declare -f $f; (($t)) && echo export -f $f || true; }; }
+show-func () { t=0 show-func-maybe-export $1; }
+export-func () { t=1 show-func-maybe-export $1; }
+run-func () { show-func $1; echo "$@"; }
+with-funcs () { echo "$@" | list | map export-func; }
+alias show=show-func shox=export-func run=run-func with=with-funcs
+
+ssh-forget-ip () { (cd; ssh-keygen -f .ssh/known_hosts -R ${1:?}); }
+ssh-learn-ip () { (cd; ssh-keyscan -H ${1:?} | tee -a .ssh/known_hosts); }
+an-ip-changed () { ssh-forget-ip $1; ssh-learn-ip $1; }
+
+func-on-a-line.awk () { awk 'NR == 1 { ++f; print; next } /; *$/ || /^{/ { --f } f == 1 { print $0 ";"; next } 1'; }
+func-on-a-line.sed () { sed -r -e 's/^ +//' -e 's/ +$//'; }
+func-on-a-line () { show-func $1 | tac | func-on-a-line.awk | func-on-a-line.sed | tac | args; echo; }
+alias short=func-on-a-line
 list-all-func () { declare -F | awk '{ print $NF }'; }
 
-(($#)) && "$@" && exit 0
+show-all-func () { list-all-func | map show-func; }
+show-all-func-on-a-line () { list-all-func | map func-on-a-line; }
+export-all-func () { list-all-func | args | apply echo export -f; }
 
-list-all-func | map func-on-a-line
-list-all-func | args | apply echo export -f
+(($#)) && { "$@"; exit $?; }
+
+show-all-func-on-a-line
+export-all-func
 alias
